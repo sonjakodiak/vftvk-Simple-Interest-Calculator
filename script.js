2d40876ca41b75ce30c08d8fd5c8a81fa7f85540
Sonja K <107422416+sonjakodiak@users.noreply.github.com>
2022-06-25 11:12:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+/* I'm sorry. I did a lot of trying on this through jsfiddle, and I couldn't get it to work at all. I am really confused between:
+variables, id's, function names, function results, and invoking functions. More study and practice needed!*/
+
+var amount = document.getElementById("amount");
+var rate = document.getElementById("rate");
+var years = document.getElementById("years");
+
+var result = Compute(amount, rate, years);
+document.getElementById("result").innerHTML = result;
+
+function Compute(amount, rate, years) {
+  return result=amount* (1 + (rate / 100) years)**years
 }
-        
