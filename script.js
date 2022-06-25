b444527b315060378ade2468c66a872d00489b2f
Sonja K <107422416+sonjakodiak@users.noreply.github.com>
2022-06-25 11:14:53 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 /* I'm sorry. I did a lot of trying on this through jsfiddle, and I couldn't get it to work at all. I am really confused between:
-variables, id's, function names, function results, and invoking functions. More study and practice needed!*/
+variables, id's, function names, function results, and invoking functions. No matter what I did, I could not get the simplest item to show up for the
+onclick event.
+More study and practice needed!*/
 
 var amount = document.getElementById("amount");
 var rate = document.getElementById("rate");
@@ -9,5 +11,5 @@ var result = Compute(amount, rate, years);
 document.getElementById("result").innerHTML = result;
 
 function Compute(amount, rate, years) {
-  return result=amount* (1 + (rate / 100) years)**years
+  return result=amount* (1 + (rate / 100) years)**years;
 }
